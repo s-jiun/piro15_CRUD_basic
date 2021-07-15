af07d9a585b1119396b1a35bba6b4743214d696f
Jiun <jiun48562@gmail.com>
2021-07-15 15:52:17 +0900

    CRUD

--- CRUD/Posts/urls.py
+++ CRUD/Posts/urls.py
@@ -6,4 +6,7 @@ app_name = 'Posts'
 urlpatterns = [
     path('', view = views.post_list, name='list'),
     path('<int:pk>/', view=views.post_detail, name='detail'),
+    path('create/', view=views.post_create, name='create'),
+    path('<int:pk>/update/', view=views.post_update, name='update'),
+    path('<int:pk>/delete/', view=views.post_delete, name='delete'),
 ]
--- CRUD/Posts/views.py
+++ CRUD/Posts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from .forms import PostForm
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
@@ -14,3 +16,34 @@ def post_detail(request, pk):
     ctx = {'post': post}
 
     return render(request, template_name='detail.html', context=ctx)
+
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post=form.save()
+            return redirect('Posts:list')
+    else:
+        form = PostForm()
+        ctx = {'form': form}
+
+        return render(request, template_name='post_form.html', context=ctx)
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, id=pk)
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('Posts:detail', pk)
+    else:
+        form = PostForm(instance=post)
+        ctx = {'form': form}
+
+    return render(request, template_name='post_form.html', context=ctx)
+
+def post_delete(request, pk):
+    post = Post.objects.get(id=pk)
+    post.delete()
+    return redirect('Posts:list')
